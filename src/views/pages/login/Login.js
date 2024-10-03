ad27ae5ccb2e56b099306057239e649d4f9d4a59
CarlUlarte <126319539+CarlUlarte@users.noreply.github.com>
2024-10-03 12:18:28 +0800

    removed sign up section

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -18,13 +18,13 @@ import { cilLockLocked, cilUser } from '@coreui/icons'
 
 const Login = () => {
   return (
-    <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
+    <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center justify-content-center">
       <CContainer>
         <CRow className="justify-content-center">
           <CCol md={8}>
             <CCardGroup>
               <CCard className="p-4">
-                <CCardBody>
+                <CCardBody className="text-center"> {/* Center the text */}
                   <CForm>
                     <h1>Login</h1>
                     <p className="text-body-secondary">Sign In to your account</p>
@@ -44,35 +44,21 @@ const Login = () => {
                         autoComplete="current-password"
                       />
                     </CInputGroup>
-                    <CRow>
-                      <CCol xs={6}>
+                    <CRow className="mb-3"> {/* New row for the login button */}
+                      <CCol className="d-flex justify-content-center">
                         <CButton color="primary" className="px-4">
                           Login
                         </CButton>
                       </CCol>
-                      <CCol xs={6} className="text-right">
+                    </CRow>
+                    <CRow> {/* New row for the forgot password link */}
+                      <CCol className="d-flex justify-content-center">
                         <CButton color="link" className="px-0">
                           Forgot password?
                         </CButton>
                       </CCol>
                     </CRow>
                   </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
                 </CCardBody>
               </CCard>
             </CCardGroup>
